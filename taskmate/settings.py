f75aeef27eb01c16bea4b70e87ee071b5f116dbe
PranavTupe2000 <iamgroot5820@gmail.com>
2021-03-31 15:38:22 +0530

    gunicorn setup

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
