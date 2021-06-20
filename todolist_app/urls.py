ba79ee29f849552172a741fdf111f10d8df617bc
PranavTupe2000 <iamgroot5820@gmail.com>
2021-06-20 19:26:04 +0530

    Mark as Complete/Pending fixed

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.todolist, name='todolist'),
     path('delete/<task_id>', views.delete_task, name='delete_task'),
     path('edit/<task_id>', views.edit_task, name='edit_task'),
+    path('complete/<task_id>', views.complete_task, name='complete_task'),
+    path('pending/<task_id>', views.pending_task, name='pending_task'),
     path('contact/', views.contact, name='contactus'),
     path('about/', views.about, name='aboutus'),
 ]
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -52,3 +52,19 @@ def edit_task(request,task_id):
     else:
         task_obj= TaskList.objects.get(pk=task_id)
         return render(request,'edit.html',{'task_obj':task_obj})
+
+@login_required
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+
+    return redirect('todolist')
+
+@login_required
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+
+    return redirect('todolist')
